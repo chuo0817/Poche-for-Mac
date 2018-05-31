b3c9762659eca2708321e683e70444818d1355f5
Chuo Qu <qu.chuo@advance.ai>
2018-05-31 15:09:10 +0800

    add getitem api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+  componentDidMount() {
+    axios.get('/playlists/',{headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    }}).then(res => {
+      const items = res.data
+      this.setState({items})
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>破车推荐</h1>
+          <ul>
+            {this.state.items.map( i =>
+              <li key={i.id}>{i.title}</li>
+            )}
+          </ul>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Title from './Title'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App /> ,document.getElementById('root'));
 registerServiceWorker();
