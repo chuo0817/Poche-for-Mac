9da1b258459bdb0c2ef40d6eadfeb3431ad012fd
Chuo Qu <qu.chuo@advance.ai>
2018-06-08 11:43:37 +0800

    complete index style and so on

---
+++ src/containers/detail/detail.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+class detail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+    }
+  }
+  componentDidMount() {
+    axios
+      .get('/playlists/', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(res => {
+        const items = res.data
+        this.setState({ items })
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>破车推荐</h1>
+          <div>
+            {this.state.items.map(i => (
+              <div className="item" style={{ backgroundImage: `url(${i.cover})` }}
+                key={i.id}> {i.title} </div>
+            ))}
+          </div>
+        </header>
+      </div>
+    )
+  }
+}
