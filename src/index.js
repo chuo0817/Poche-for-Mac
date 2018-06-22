432cc99bb0b2bfe4c5444d1efbdbc60bfd972d9a
Chuo Qu <qu.chuo@advance.ai>
2018-06-22 18:49:31 +0800

    leave route problem

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './containers/catalog/App';
 import Detail from './containers/detail/detail'
-import { Router, Route } from 'react-router'
-import { Link } from 'react-router-dom'
+// import { Router, Route } from 'react-router'
+// import { Link } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-    <Router>
-      <Route path="/" component={App}>
-        <Route path="detail" component={Detail} />
-      </Route>
-    </Router>
+    // <Router>
+    //   <Route path="/" component={App}>
+    //     <Route path="detail" component={Detail} />
+    //   </Route>
+    // </Router>
+    <App />
   ) ,document.getElementById('root'));
 registerServiceWorker();
