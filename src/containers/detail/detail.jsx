0c25548d8c5f638a4b7454f384f09fdefe949466
Chuo Qu <qu.chuo@advance.ai>
2018-08-14 14:30:19 +0800

    update

--- src/containers/detail/detail.jsx
+++ src/containers/detail/detail.jsx
@@ -3,8 +3,8 @@ import axios from 'axios'
 import AudioPlayer from 'react-responsive-audio-player'
 
 
-const playlist = []
-
+let playlist = []
+const controls = ['playpause', 'forwardskip', 'progressdisplay']
 class Detail extends React.Component {
   constructor(props) {
     super(props)
@@ -14,28 +14,39 @@ class Detail extends React.Component {
   }
   // this.props.params.query
   componentDidMount() {
-    console.log(this.props)
+    console.log()
     axios
-      .get(`/playlists`, {
+      .get(`/playlists/${this.props.location.pathname.substring(8)}`, {
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
         },
       })
       .then(res => {
-        const items = res.data
+        const items = res.data.map(i => {
+          return {
+            id: i.id,
+            cover: i.cover,
+            artist: i.artist,
+            title: i.name,
+            url: i.url
+          }
+        })
+        playlist = items
+        console.log(playlist)
         this.setState({ items })
       })
   }
 
-
   render() {
+  console.log('asdf',playlist)
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1>破车推荐detail</h1>
+          <h1>detail</h1>
         </header>
-        <AudioPlayer playlist={playlist} />,
+        <AudioPlayer playlist={playlist} controls={controls}  autoplay={true}  />,
       </div>
     )
   }
