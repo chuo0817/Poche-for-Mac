c430fc61bfbebfb80983288f6666533c713ec8d1
Chuo Qu <qu.chuo@advance.ai>
2018-08-14 18:18:13 +0800

    add logo icon1

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 800, height: 600,icon: __dirname + '/logo.ico' })
 
   // and load the index.html of the app.
   if (pkg.DEV) {
