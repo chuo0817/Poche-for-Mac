4fd8b9c773e73394f9f7f2c2b3a1709be13bf7d0
Rachel <rach31ch1u@gmail.com>
2018-11-26 12:55:06 +0800

    delete detail quote in indexJs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './containers/catalog/App.jsx';
-import Detail from './containers/detail/detail.jsx'
 import { Route } from 'react-router'
 import { HashRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
@@ -11,7 +10,6 @@ ReactDOM.render((
 	<HashRouter>
 		<div>
 	        <Route exact path="/" component={App} />
-	        <Route path="/detail" component={Detail} />
     </div>
 	</HashRouter>
 ), document.getElementById('root'));
