ed19ac00b31d67143910823bedb2a151cff446ad
Rachel <rach31ch1u@gmail.com>
2018-11-28 22:19:47 +0800

    rebuild app component and list component

--- src/containers/catalog/App.jsx
+++ src/containers/catalog/App.jsx
@@ -4,47 +4,35 @@ import axios from 'axios'
 // import { Link } from 'react-router-dom'
 import Player from '../player/Player.jsx'
 import List from '../list/List.jsx'
-import { fetchMusic, fetchAlbumList } from '../../util/request'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentMusicList: [],
-      playingMusic: {},
-      albumList: [],
-      musicList: [],
-      childIndex: 0,
-      music: {},
-      index: 0,
+      playingMusic: {}
     }
   }
 
   componentDidMount() {
-    fetchAlbumList(list => {
-      this.setState({ albumList: list })
-      const firstAlbumId = list[0].id;
-      fetchMusic(firstAlbumId, music => {
-        this.setState({ musicList: music })
-        this.setState({ music: music[this.state.index] })
-        // this.props.onSelectedMusic(musics[0])
-      })
-    })
   }
 
   render() {
     return (
       <div className="App">
-        <Player music={this.state.musicList[this.state.index]} playNextMusic={this.onPlayNextMusic} playPreviousMusic={this.onPlayPreviousMusic}></Player>
-        <List albumlist={this.state.albumList} musiclist={this.state.musicList} indexfn={this.getChildIndex}></List>
-      </div>
+        <Player music={this.state.playingMusic} playNextMusic={this.onPlayNextMusic} playPreviousMusic={this.onPlayPreviousMusic}></Player>
+        <List onChangeMusic={this.onChangeMusic}></List>
+      </div >
     )
+  }
+
+  onChangeMusic = (music) => {
+    this.setState({ playingMusic: music })
   }
 
   onPlayerMusic = (music) => {
     this.setState({ playingMusic: music })
   }
-  onPlayPreviousMusic =() => {
+  onPlayPreviousMusic = () => {
     this.setState({ index: --this.state.index })
   }
   onPlayNextMusic = (repeat) => {
--- src/containers/list/List.jsx
+++ src/containers/list/List.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import './List.css'
-import { fetchMusic } from '../../util/request'
+import { fetchMusic, fetchAlbumList } from '../../util/request'
 
 
 class List extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      albums: [],
-      items: []
+      playingMusicList: [],
+      previewMusicList: [],
+      albumList: [], 
+      playingAlbum: {},
+      previewAlbum: {}
     }
   }
 
+  componentDidMount() {
+    const index = 0
+    fetchAlbumList(list => {
+      console.log(list);
+      this.setState({ albumList: list, previewAlbum: list[index] })
+      const firstAlbumId = list[index].id;
+      fetchMusic(firstAlbumId, musicList => {
+        this.setState({ previewMusicList: musicList, playingMusicList: musicList, playingAlbum: list[index] })
+        this.props.onChangeMusic(musicList[0])
+      })
+    })
+  }
+
   componentWillReceiveProps() {
-    this.setState({ items: this.props.musiclist })
+    // this.setState({ items: this.props.musiclist.list })
   }
 
   render() {
@@ -22,29 +38,29 @@ class List extends React.Component {
       <div className="list-container">
         <div className="music-list">
           <div className="header">
-            <span>破车推荐2018年4月号</span>
+            <span>{this.state.previewAlbum.title}</span>
           </div>
           <div className="list">
-            {this.state.items.map((music, index) => (
+            {this.state.previewMusicList.map((music, index) => (
               <div className={`item ${index === this.state.index ? 'active' : ''}`}
-              key={music.id} onDoubleClick={this.handleSelectedMusic.bind(this, music, index)}>
-              <div className="content">
-                <span className="cover"><img src={music.cover} alt="" /></span>
-                <div className="info">
-                  <div className="title">{music.title}</div>
-                  <div className="artist">{music.artist}</div>
+                key={music.id} onDoubleClick={this.handleSelectedMusic.bind(this, music, index)}>
+                <div className="content">
+                  <span className="cover"><img src={music.cover} alt="" /></span>
+                  <div className="info">
+                    <div className="title">{music.title}</div>
+                    <div className="artist">{music.artist}</div>
+                  </div>
                 </div>
               </div>
-            </div>
             ))}
           </div>
         </div>
         <div className="albumlist">
           <div className="list">
-            {this.props.albumlist.map((album, index) => (
+            {this.state.albumList.map((album, index) => (
               <div className={`item ${index === this.state.index ? 'active' : ''}`}
                 style={{ backgroundImage: `url(${album.cover})` }} key={album.id}
-                onClick={this.handlePreviewAlbum.bind(this, album.id)}>
+                onClick={this.handlePreviewAlbum.bind(this, index)}>
                 <span className="title">{album.title}</span>
               </div>
             ))}
@@ -55,14 +71,26 @@ class List extends React.Component {
   }
 
   handleSelectedMusic = (music, index) => {
-    this.props.indexfn(index)
+    // console.log(this.state.previewAlbum, this.state.playingAlbum);
+    
+    if (this.state.previewAlbum.id === this.state.playingAlbum.id) {
+      const music = this.state.playingMusicList[index]
+      this.props.onChangeMusic(music)
+    } else {
+      const music = this.state.previewMusicList[index]
+      this.setState({ playingAlbum: this.state.previewAlbum, playingMusicList: this.state.previewMusicList })
+      this.props.onChangeMusic(music)
+    }
+    // console.log(music, index);
+    
+    // this.props.
   }
 
-  handlePreviewAlbum = (id) => {
-    fetchMusic(id, music => {
-      this.setState({ items: music })
+  handlePreviewAlbum = (index) => {
+    const selectedAlbum = this.state.albumList[index]
+    fetchMusic(selectedAlbum.id, musicList => {
+      this.setState({ previewMusicList: musicList, previewAlbum: selectedAlbum })
     })
-
   }
 }
 export default List
