cabf46e0d5a6d33665474d4b037e70e33700a171
Rachel <rach31ch1u@gmail.com>
2018-12-21 00:33:03 +0800

    change the remote origin

--- src/util/request.js
+++ src/util/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const $http = axios.create({
-  baseURL: 'http://localhost:3000/api/app'
+  baseURL: 'https://poche.fm/api/app'
 })
 
 export function fetchAlbumList(cb) {
